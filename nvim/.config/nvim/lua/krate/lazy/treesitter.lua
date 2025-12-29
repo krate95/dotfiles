5137bd14e6775df7eecae19f6e5a562ff2744cf8
Jesús Elvira <j.elvirapiqueras@mnemo.com>
2025-12-29 11:56:05 +0100

    Add Neovim configuration files for various plugins

--- nvim/.config/nvim/lua/krate/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/krate/lazy/treesitter.lua
@@ -1,22 +1,35 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
 
-    configs.setup({
-      ensure_installed = {
-          "lua", "vim", "vimdoc", "bash",
-          "json", "yaml",
-          "html", "css",
-          "javascript", "typescript", "tsx",
-          "markdown", "markdown_inline",
-          "dockerfile", "gitignore",
-          "regex", "query",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+    -- Install parsers asynchronously. If they are already installed, it does nothing.
+      require("nvim-treesitter").install({
+        "lua", "vim", "vimdoc", "bash",
+        "json", "yaml",
+        "html", "css",
+        "javascript", "typescript", "tsx",
+        "markdown", "markdown_inline",
+        "dockerfile", "gitignore",
+        "regex", "query",
+      })
+
+    -- Enable highlighting per filetype (Neovim provides the highlighting)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "lua","vim","bash","json","yaml","html","css",
+          "javascript","typescript","typescriptreact",
+          "markdown","dockerfile","gitignore",
+        },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
+  },
 }
