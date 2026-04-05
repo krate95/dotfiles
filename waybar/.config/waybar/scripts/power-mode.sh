4e9a04881ed28805bf6258c0fac39c5819e9d1c3
krate95 <jesus200495@gmail.com>
2026-04-05 17:50:56 +0200

    waybar: translate power-mode tooltips to English

--- waybar/.config/waybar/scripts/power-mode.sh
+++ waybar/.config/waybar/scripts/power-mode.sh
@@ -4,7 +4,7 @@
 AC_ONLINE=$(cat /sys/class/power_supply/AC*/online 2>/dev/null | head -1)
 
 if [ "$AC_ONLINE" = "1" ]; then
-    echo '{"text":"󱐋 AC","class":"ac","tooltip":"TLP: modo rendimiento (AC)"}'
+    echo '{"text":"󱐋 AC","class":"ac","tooltip":"TLP: performance mode"}'
 else
-    echo '{"text":"󰁹 BAT","class":"bat","tooltip":"TLP: modo ahorro (batería)"}'
+    echo '{"text":"󰁹 BAT","class":"bat","tooltip":"TLP: battery save mode"}'
 fi
