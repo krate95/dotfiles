63532d8fce46b2d8902a3bd2e19e187c7b8bb2d4
krate95 <jesus200495@gmail.com>
2026-04-06 10:47:12 +0200

    osd: replace dunst OSD with swayosd, add capslock and playerctl
    - Move notifications to top-right in dunst, remove volume/brightness rules
    - Replace wob with swayosd-server for volume, brightness, playerctl and capslock OSD
    - Simplify volume.sh and brightness.sh to delegate to swayosd-client
    - Add Catppuccin Mocha CSS theme for swayosd
    - Add wob config (kept as fallback)

--- waybar/.config/waybar/scripts/brightness.sh
+++ waybar/.config/waybar/scripts/brightness.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-# Fixed notification ID to replace previous notification
-NOTIF_ID=9992
-
 action="$1"
 
 case "$action" in
-    up)
-        brightnessctl -n2 set 5%+
-        ;;
-    down)
-        brightnessctl -n2 set 5%-
-        ;;
+    up)   swayosd-client --brightness +5 ;;
+    down) swayosd-client --brightness -5 ;;
 esac
-
-# Read current brightness
-BRIGHTNESS=$(brightnessctl get)
-MAX=$(brightnessctl max)
-PCT=$(( BRIGHTNESS * 100 / MAX ))
-
-if [ "$PCT" -ge 67 ]; then
-    ICON="󰃠"
-elif [ "$PCT" -ge 34 ]; then
-    ICON="󰃟"
-else
-    ICON="󰃞"
-fi
-
-dunstify -a "brightness" -u low -r "$NOTIF_ID" \
-    -h "int:value:$PCT" \
-    "$ICON  Brightness" "${PCT}%"
--- waybar/.config/waybar/scripts/volume.sh
+++ waybar/.config/waybar/scripts/volume.sh
@@ -1,44 +1,10 @@
 #!/bin/bash
 
-# Fixed notification ID to replace previous notification
-NOTIF_ID=9991
-
 action="$1"
 
 case "$action" in
-    up)
-        wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-        ;;
-    down)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-        ;;
-    mute)
-        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        ;;
-    mute-mic)
-        wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-        ;;
+    up)       swayosd-client --output-volume +5 --max-volume 100 ;;
+    down)     swayosd-client --output-volume -5 ;;
+    mute)     swayosd-client --output-volume mute-toggle ;;
+    mute-mic) swayosd-client --input-volume mute-toggle ;;
 esac
-
-# Read current state
-MUTED=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -c MUTED)
-VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f", $2 * 100}')
-
-if [ "$MUTED" -gt 0 ]; then
-    ICON="󰝟"
-    MSG="Muted"
-    dunstify -a "volume" -u low -r "$NOTIF_ID" \
-        -h "int:value:$VOL" \
-        "$ICON  Volume" "$MSG" &
-else
-    if [ "$VOL" -ge 67 ]; then
-        ICON="󰕾"
-    elif [ "$VOL" -ge 34 ]; then
-        ICON="󰖀"
-    else
-        ICON="󰕿"
-    fi
-    dunstify -a "volume" -u low -r "$NOTIF_ID" \
-        -h "int:value:$VOL" \
-        "$ICON  Volume" "${VOL}%" &
-fi
