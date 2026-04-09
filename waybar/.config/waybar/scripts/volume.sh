e669978c88561deb819c7ea880cd8d25ecf18d88
krate95 <jesus200495@gmail.com>
2026-04-09 13:43:06 +0200

    fix mic mute toggle not re-enabling

--- waybar/.config/waybar/scripts/volume.sh
+++ waybar/.config/waybar/scripts/volume.sh
@@ -6,5 +6,5 @@ case "$action" in
     up)       swayosd-client --output-volume +5 --max-volume 100 ;;
     down)     swayosd-client --output-volume -5 ;;
     mute)     swayosd-client --output-volume mute-toggle ;;
-    mute-mic) swayosd-client --input-volume mute-toggle ;;
+    mute-mic) pactl set-source-mute @DEFAULT_SOURCE@ toggle ;;
 esac
